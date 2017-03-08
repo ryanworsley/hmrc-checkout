834442c3bff007d40a7c160164a89248db0d8acb
Ryan Worsley <ryan@transcognify.com>
2017-03-08 18:31:20 +0000

    Added Step 2, ability to inject a discounter service (function), refactored to use this and for readability

--- src/main/scala/com/hmrc/checkout/services/Checkout.scala
+++ src/main/scala/com/hmrc/checkout/services/Checkout.scala
@@ -1,19 +1,55 @@
 package com.hmrc.checkout.services
 
-class Checkout(priceLookup: String => Option[BigDecimal]) {
-  def price(items: Seq[String]): (BigDecimal, Set[String]) = {
-    val (found, notFound) = items
-      .map(item => (item, priceLookup(item)))
-      .partition {
-        case (_, price) => price.isDefined
-      }
+import com.hmrc.checkout.services.Checkout.{Item, Price, Quantity}
 
-    val total = found.foldLeft(BigDecimal(0)) {
-      case (runningTotal, (_, Some(price))) => runningTotal + price
+class Checkout(priceLookup: Item => Option[Price], discounter: String => (Quantity, Price) => Price) {
+  def price(items: Seq[Item]): (Price, Set[Item]) = {
+    val (found, notFound) = lookupPrices(items)
+    val quantityAndPriceByItem = totalQuantities(found)
+    val priceAndDiscountByItem = getTotalsAndDiscounts(quantityAndPriceByItem)
+
+    val total = priceAndDiscountByItem.foldLeft(BigDecimal(0)) {
+      case (runningTotal, (_, (price, discount))) => runningTotal + price - discount
     }
 
-    val notFoundItems = notFound.map { case (item, _) => item }.toSet
-
-    (total, notFoundItems)
+    (total, notFound)
   }
+
+  private def lookupPrices(items: Seq[Item]) = items
+    .map(item => (item, priceLookup(item)))
+    .partition {
+      case (_, price) => price.isDefined
+    } match {
+      case (found, notFound) =>
+        val foundWithPrice = found.map {
+          case (item, Some(price)) => (item, price)
+          case (item, _) => (item, BigDecimal(0))
+        }
+
+        val itemsNotFound = notFound.map {
+          case (item, _) => item
+        }
+
+        (foundWithPrice, itemsNotFound.toSet)
+    }
+
+  private def totalQuantities(pricedItems: Seq[(Item, Price)]) = pricedItems.groupBy {
+    case ((item, _)) => item
+  }.mapValues {
+    case items @ (_, price) :: _ => (items.length, price)
+  }
+
+  private def getTotalsAndDiscounts(quantityAndPriceByItem: Map[Item, (Quantity, Price)]) = quantityAndPriceByItem.map {
+    case (item, (quantity, price)) =>
+      val total: Price = quantity * price
+      val discount: Price = discounter(item.toLowerCase.trim)(quantity, price)
+
+      item -> (total, discount)
+  }
+}
+
+object Checkout {
+  type Quantity = Int
+  type Price = BigDecimal
+  type Item = String
 }
--- src/test/scala/com/hmrc/checkout/test/services/CheckoutTests.scala
+++ src/test/scala/com/hmrc/checkout/test/services/CheckoutTests.scala
@@ -5,10 +5,19 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class CheckoutTests extends FlatSpec with Matchers {
   private val prices = Map("apple" -> BigDecimal(60), "orange" -> BigDecimal(25))
+  private val priceLookup: String => Option[BigDecimal] = item => prices.get(item.toLowerCase.trim)
+
+  private val discountFunctions = Map[String, (Int, BigDecimal) => BigDecimal](
+    "apple" -> ((quantity, price) => if (quantity > 1) (quantity % 2) * price else 0),
+    "orange" -> ((quantity, price) => Math.floor(quantity / 3) * price)
+  )
+
+  private val discounter: String => (Int, BigDecimal) => BigDecimal =
+    discountFunctions.getOrElse(_, (_, _) => BigDecimal(0))
 
   it should "return nothing for an empty basket" in {
     val items = Seq()
-    val checkout = new Checkout(item => prices.get(item.toLowerCase.trim))
+    val checkout = new Checkout(priceLookup, discounter)
 
     val (total, notFound) = checkout.price(items)
 
@@ -18,7 +27,7 @@ class CheckoutTests extends FlatSpec with Matchers {
 
   it should "calculate the price of items" in {
     val items = Seq("Apple", "Orange")
-    val checkout = new Checkout(item => prices.get(item.toLowerCase.trim))
+    val checkout = new Checkout(priceLookup, discounter)
 
     val (total, notFound) = checkout.price(items)
 
@@ -28,7 +37,7 @@ class CheckoutTests extends FlatSpec with Matchers {
 
   it should "handle repeated items" in {
     val items = Seq("Apple", "Orange", "Apple")
-    val checkout = new Checkout(item => prices.get(item.toLowerCase))
+    val checkout = new Checkout(priceLookup, discounter)
 
     val (total, notFound) = checkout.price(items)
 
@@ -38,11 +47,31 @@ class CheckoutTests extends FlatSpec with Matchers {
 
   it should "return a list of unknown items" in {
     val items = Seq("Apple", "Hedgehog")
-    val checkout = new Checkout(item => prices.get(item.toLowerCase.trim))
+    val checkout = new Checkout(priceLookup, discounter)
 
     val (total, notFound) = checkout.price(items)
 
     total should be (60)
     notFound should be (Set("Hedgehog"))
+  }
+
+  it should "apply discount functions (buy one get one free)" in {
+    val items = Seq("Apple", "Apple", "Apple")
+    val checkout = new Checkout(priceLookup, discounter)
+
+    val (total, notFound) = checkout.price(items)
+
+    total should be (120)
+    notFound should be (Set.empty)
+  }
+
+  it should "apply discount functions (3 for 2)" in {
+    val items = Seq("Orange", "Orange", "Orange")
+    val checkout = new Checkout(priceLookup, discounter)
+
+    val (total, notFound) = checkout.price(items)
+
+    total should be (50)
+    notFound should be (Set.empty)
   }
 }
