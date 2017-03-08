fb4cd1ce3deb006f396fc6b8de708ba126988e2d
Ryan Worsley <ryan@transcognify.com>
2017-03-08 17:01:49 +0000

    Rewrote slightly to support how I intend to do discounting

--- src/main/scala/com/hmrc/checkout/services/Checkout.scala
+++ src/main/scala/com/hmrc/checkout/services/Checkout.scala
@@ -1,13 +1,19 @@
 package com.hmrc.checkout.services
 
 class Checkout(priceLookup: String => Option[BigDecimal]) {
-  def price(items: Seq[String]): (BigDecimal, Set[String]) =
-    items.foldLeft((BigDecimal(0), Set.empty[String]))((acc, item) => {
-      val (runningTotal, notFound) = acc
-
-      priceLookup(item) match {
-        case Some(price) => (runningTotal + price, notFound)
-        case None => (runningTotal, notFound + item)
+  def price(items: Seq[String]): (BigDecimal, Set[String]) = {
+    val (found, notFound) = items
+      .map(item => (item, priceLookup(item)))
+      .partition {
+        case (_, price) => price.isDefined
       }
-    })
+
+    val total = found.foldLeft(BigDecimal(0)) {
+      case (runningTotal, (_, Some(price))) => runningTotal + price
+    }
+
+    val notFoundItems = notFound.map { case (item, _) => item }.toSet
+
+    (total, notFoundItems)
+  }
 }
